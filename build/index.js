c07b72281e47d203d289ee0d97efff403e402d01
Vladislav Korenik <vladislavkorenik@gmail.com>
2020-02-05 19:11:28 +0300

    added function for resolvers

--- build/index.js
+++ build/index.js
@@ -435,6 +435,38 @@ const resolvers = {
       id
     }, context, info) => _db__WEBPACK_IMPORTED_MODULE_0__["users"].find(user => user.id == id),
     users: (parent, args, context, info) => _db__WEBPACK_IMPORTED_MODULE_0__["users"]
+  },
+  Mutation: {
+    createUser: (parent, {
+      id,
+      name,
+      email,
+      age
+    }, context, info) => {
+      const newUser = {
+        id,
+        name,
+        email,
+        age
+      };
+      _db__WEBPACK_IMPORTED_MODULE_0__["users"].push(newUser);
+      return newUser;
+    },
+    updateUser: (parent, newDate, context, info) => {
+      let newUser = _db__WEBPACK_IMPORTED_MODULE_0__["users"].find(user => user.id == newDate.id);
+      newUser.name = newDate.name;
+      newUser.email = newDate.email;
+      newUser.age = newDate.age;
+      return newUser;
+    },
+    deleteUser: (parent, {
+      id
+    }, context, info) => {
+      const userIndex = _db__WEBPACK_IMPORTED_MODULE_0__["users"].findIndex(user => user.id == id);
+      if (userIndex === -1) throw new Error("User not found.");
+      const deletedUsers = _db__WEBPACK_IMPORTED_MODULE_0__["users"].splice(userIndex, 1);
+      return deletedUsers[0];
+    }
   }
 };
 /* harmony default export */ __webpack_exports__["default"] = (resolvers);
--- src/resolvers.js
+++ src/resolvers.js
@@ -4,6 +4,33 @@ const resolvers = {
   Query: {
     user: (parent, { id }, context, info) => users.find(user => user.id == id),
     users: (parent, args, context, info) => users
+  },
+  Mutation: {
+    createUser: (parent, { id, name, email, age }, context, info) => {
+      const newUser = { id, name, email, age };
+
+      users.push(newUser);
+
+      return newUser;
+    },
+    updateUser: (parent, newDate, context, info) => {
+      let newUser = users.find(user => user.id == newDate.id);
+
+      newUser.name = newDate.name;
+      newUser.email = newDate.email;
+      newUser.age = newDate.age;
+
+      return newUser;
+    },
+    deleteUser: (parent, { id }, context, info) => {
+      const userIndex = users.findIndex(user => user.id == id);
+
+      if (userIndex === -1) throw new Error("User not found.");
+
+      const deletedUsers = users.splice(userIndex, 1);
+
+      return deletedUsers[0];
+    }
   }
 };
 
